65f76c7a45dbab2fd1340a732c60032122baf842
Ankit Chadha <ankitrchadha@gmail.com>
2020-03-07 14:46:36 -0800

    Bert On by default

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ class WarfarinDosageRecommendation(object):
         return (rewards, predictions)
 
 if __name__ == '__main__':
-    data_prepocessor = DataPipeline(bert_on=False)
+    #data_prepocessor = DataPipeline(bert_on=False)
+    data_prepocessor = DataPipeline(bert_on=True)
     X_train, X_val, y_train, y_val = data_prepocessor.loadAndPrepData()
     linUCB_policy = ContextualLinearUCBPolicy(features_size=X_train.shape[1], num_actions=3)
     warfarin = WarfarinDosageRecommendation(linUCB_policy, data=(X_train, X_val, y_train, y_val))
